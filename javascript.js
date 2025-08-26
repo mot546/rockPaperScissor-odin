2daf180f43b56d6564d5153a8ad1390a8a788cd1
mot546 <jellopancake213@gmail.com>
2025-08-26 12:05:47 +0800

    Create user input

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 // the value of rock is 0, paper is 1, scissor is 2 
 
+let humanScore = 0, compScore = 0;
+
 function getComputerChoice(){// get the following number: 0 = rock, 1= paper, 2= scissor,,,,,
     const randomNum = Math.floor(Math.random() * 3);        //this function return 2 values
 
@@ -13,8 +15,35 @@ function getComputerChoice(){// get the following number: 0 = rock, 1= paper, 2=
         return ["scissor", 2];
     }
     
-}
+}// this returns array
 
 function getHumanChoice(){
-    
+    let userInput = prompt("Enter Rock, Paper, Scissor", "paper");
+    userInput = userInput.toUpperCase();
+
+    if (userInput === "ROCK" || userInput === "ROCKS"){
+        return ["rock",0];
+    }
+    else if (userInput === "PAPER" || userInputs === "PAPERS"){
+        return ["paper", 1];
+    }
+    else if (userInput === "SCISSOR" || userInput === "SCISSORS"){
+        return ["scissor", 2];
+    }
+    else {
+        alert("Input INVALID, PLEASE CHECK YOUR SPELLING!");
+        alert("Try again ok");
+
+        getHumanChoice();
+    }
+} // this return array
+
+function playRound(human, comp){
+
 }
+
+let comp = getComputerChoice();
+let human = getHumanChoice();
+
+console.log(comp);
+console.log(human);
